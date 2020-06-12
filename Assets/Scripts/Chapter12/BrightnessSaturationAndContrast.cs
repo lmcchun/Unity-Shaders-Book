b6d0afd18f1605785b4b32e847406b127579bccc
lmc_chun <lmc_chun@msn.com>
2020-06-12 15:28:00 +0800

    update.

---
+++ Assets/Scripts/Chapter12/BrightnessSaturationAndContrast.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class BrightnessSaturationAndContrast : PostEffectsBase
+{
+#pragma warning disable 0649
+    [SerializeField]
+    private Shader briSatConShader;
+
+    [SerializeField, Range(0.0f, 3.0f)]
+    private float brightness = 1.0f;
+
+    [SerializeField, Range(0.0f, 3.0f)]
+    private float saturation = 1.0f;
+
+    [SerializeField, Range(0.0f, 3.0f)]
+    private float contrast = 1.0f;
+#pragma warning restore 0649
+
+    private Material briSatConMaterial;
+
+    public Material Material
+    {
+        get
+        {
+            briSatConMaterial = CheckShaderAndCreateMaterial(briSatConShader, briSatConMaterial);
+            return briSatConMaterial;
+        }
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (Material != null)
+        {
+            Material.SetFloat("_Brightness", brightness);
+            Material.SetFloat("_Saturation", saturation);
+            Material.SetFloat("_Contrast", contrast);
+            Graphics.Blit(src, dest, Material);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
---
+++ Assets/Scripts/Chapter12/PostEffectsBase.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class PostEffectsBase : MonoBehaviour
+{
+    protected void Start()
+    {
+        CheckResources();
+    }
+
+    // Called when start
+    protected void CheckResources()
+    {
+        if (!CheckSupport())
+        {
+            NotSupported();
+        }
+    }
+
+    // Called when the platform doesn't support this effect
+    protected void NotSupported()
+    {
+        enabled = false;
+    }
+
+    // Called in CheckResources to check support on this platform
+    protected static bool CheckSupport()
+    {
+        if (!SystemInfo.supportsImageEffects/* || !SystemInfo.supportsRenderTextures*/)
+        {
+            Debug.LogWarning("This platform does not support image effects or render textures.");
+            return false;
+        }
+
+        return true;
+    }
+
+    // Called when need to create the material used by this effect
+    protected static Material CheckShaderAndCreateMaterial(Shader shader, Material material)
+    {
+        if (shader == null)
+        {
+            return null;
+        }
+        if (!shader.isSupported)
+        {
+            return null;
+        }
+        if (material && material.shader == shader)
+        {
+            return material;
+        }
+        material = new Material(shader)
+        {
+            hideFlags = HideFlags.DontSave
+        };
+        if (material)
+        {
+            return material;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
