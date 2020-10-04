6cad52efb1b590f17e4e97b168b3f7fed5f412a0
lmc_chun <lmc_chun@msn.com>
2020-10-04 16:02:56 +0800

    update.

--- Assets/Scripts/Chapter12/Bloom.cs
+++ Assets/Scripts/Chapter12/Bloom.cs
@@ -46,32 +46,26 @@ public class Bloom : PostEffectsBase
             var buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
             buffer0.filterMode = FilterMode.Bilinear;
 
+            var buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+            // ??? 是否需要设置 filterMode
+
             Graphics.Blit(src, buffer0, Material, 0);
 
             for (var i = 0; i < iterations; ++i)
             {
                 Material.SetFloat("_BlurSize", 1.0f + i * blurSpread);
 
-                var buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
-
                 // Render the vertical pass
                 Graphics.Blit(buffer0, buffer1, Material, 1);
-
-                RenderTexture.ReleaseTemporary(buffer0);
-                buffer0 = buffer1;
-                buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
-
                 // Render the horizontal pass
-                Graphics.Blit(buffer0, buffer1, Material, 2);
-
-                RenderTexture.ReleaseTemporary(buffer0);
-                buffer0 = buffer1;
+                Graphics.Blit(buffer1, buffer0, Material, 2);
             }
 
             Material.SetTexture("_Bloom", buffer0);
             Graphics.Blit(src, dest, Material, 3);
 
             RenderTexture.ReleaseTemporary(buffer0);
+            RenderTexture.ReleaseTemporary(buffer1);
         }
         else
         {
